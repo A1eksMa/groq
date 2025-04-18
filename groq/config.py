c2e602f884df280e2100b1bbba0fcd7f17d74e51
A1ex Ma <145852894+A1eksMa@users.noreply.github.com>
2025-04-18 19:19:17 +0600

    Update config.py

--- groq/config.py
+++ groq/config.py
@@ -4,12 +4,14 @@ config = dotenv_values()
 HOST="0.0.0.0"
 PORT=5555
 
-GROQ_TOKEN = config.get("GROQ_TOKEN")
-MODEL = "llama-3.3-70b-versatile"
-TEMPERATURE = 0
-MESSAGES = list()
+YOUR_SECRET_GROQ_TOKEN = config.get("GROQ_TOKEN")
 
-groq = {"GROQ_TOKEN" : GROQ_TOKEN,
-        "MODEL" : MODEL,
-        "TEMPERATURE" : TEMPERATURE,
-        "MESSAGES" : MESSAGES}
+default_groq = {"YOUR_SECRET_GROQ_TOKEN" : YOUR_SECRET_GROQ_TOKEN,
+            "MODEL" : "llama-3.3-70b-versatile",
+            "MESSAGES" : list(),
+            "TEMPERATURE" : 1,
+	    "MAX_COMPLETION_TOKENS": 1024,
+	    "TOP_P": 1,
+            "STREAM": False,
+            "STOP": None,
+           }
