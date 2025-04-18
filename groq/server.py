d92f022b58ad8d408d6f72fc1181a4f1c560d8a8
A1ex Ma <145852894+A1eksMa@users.noreply.github.com>
2025-04-18 19:22:18 +0600

    Update server.py

--- groq/server.py
+++ groq/server.py
@@ -2,45 +2,36 @@ from groq import Groq
 from fastapi import FastAPI
 import uvicorn
 from config import HOST, PORT
-from config import GROQ_TOKEN, MODEL, TEMPERATURE, MESSAGES
-from config import groq
+from config import default_groq
+from config import YOUR_SECRET_GROQ_TOKEN
 
 app = FastAPI()
-messages = list()
 
-def response(groq: dict):
-    answer = Groq(api_key=groq["GROQ_TOKEN"])\
-	         .chat\
-	         .completions\
-	         .create(model=groq["MODEL"],
-	                 messages=groq["MESSAGES"],
-     		         temperature=groq["TEMPERATURE"])\
-             .choices[0]\
-	         .message\
-	         .content
-    return answer
 
-@app.post("/groq_api")
-def groq_api(groq: dict):
-    return response(groq)
-    
-@app.get("/groq_single_prompt")
-def groq_single_prompt(prompt: str, groq=groq):
-    groq["MESSAGES"].append({'role' : 'user', 'content' : prompt})
-    answer = response(groq)
-    groq["MESSAGES"] = list()
-    return answer
-
-@app.get("/groq_chat")
-def groq_chat_10_messages(prompt: str, groq=groq):
-    global messages
-    messages.append({'role' : 'user', 'content' : prompt})
-    groq["MESSAGES"] = messages
-    answer = response(groq)
-    messages.append({'role' : 'assistant', 'content' : answer})
-    groq["MESSAGES"] = list()
-    if len(messages) > 10: messages = messages[-10:]
-    return answer
+@app.post("/")
+def groq_api(groq: dict = default_groq):
+    """
+    groq = {"YOUR_SECRET_GROQ_TOKEN" : YOUR_SECRET_GROQ_TOKEN,
+            "MODEL" : "llama-3.3-70b-versatile",
+            "MESSAGES" : list(),
+            "TEMPERATURE" : 1,
+	    "MAX_COMPLETION_TOKENS": 1024,
+	    "TOP_P": 1,
+            "STREAM": False,
+            "STOP": None,
+           }
+    """
+    client = Groq(api_key=groq["YOUR_SECRET_GROQ_TOKEN"])
+    completion = client.chat.completions.create(
+	model=groq["MODEL"],
+	messages=groq["MESSAGES"],
+     	temperature=groq["TEMPERATURE"],
+	max_completion_tokens=groq["MAX_COMPLETION_TOKENS"]
+	top_p=groq["TOP_P"],
+	stream=groq["STREAM"],
+	stop=groq["STOP"],
+    )
+    return completion.choices[0].message#.content
 
 if __name__=="__main__":
     uvicorn.run(app, host=HOST, port=PORT)
