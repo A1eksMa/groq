f7c1d3290029427370f5a7154770bd9333ab390c
A1ex Ma <145852894+A1eksMa@users.noreply.github.com>
2025-04-18 18:11:23 +0600

    Update config.py

--- groq/config.py
+++ groq/config.py
@@ -1,7 +1,10 @@
+from dotenv import dotenv_values
+config = dotenv_values()
+
 HOST="0.0.0.0"
 PORT=5555
 
-GROQ_TOKEN = "YOUR_SECRET_GROQ_TOKEN"
+GROQ_TOKEN = config.get("GROQ_TOKEN")
 MODEL = "llama-3.3-70b-versatile"
 TEMPERATURE = 0
 MESSAGES = list()
