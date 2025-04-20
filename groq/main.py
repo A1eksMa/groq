0d1c6d2758cbf5b9b302989fd55b7d0654e1bed8
A1ex Ma <145852894+A1eksMa@users.noreply.github.com>
2025-04-20 23:58:44 +0600

    Update and rename server.py to main.py

--- groq/main.py
+++ groq/main.py
@@ -11,6 +11,8 @@ app = FastAPI()
 @app.post("/")
 def groq_api(groq: dict = default_groq):
     """
+    Example of usage.
+    Set `groq` variable:
     groq = {"YOUR_SECRET_GROQ_TOKEN" : YOUR_SECRET_GROQ_TOKEN,
             "MODEL" : "llama-3.3-70b-versatile",
             "MESSAGES" : list(),
@@ -21,20 +23,20 @@ def groq_api(groq: dict = default_groq):
             "STOP": None,
            }
     """
-    if not groq["STREAM"]:
-	client = Groq(api_key=groq["YOUR_SECRET_GROQ_TOKEN"])
-	completion = client.chat.completions.create(
-	model=groq["MODEL"],
-	messages=groq["MESSAGES"],
-	temperature=groq["TEMPERATURE"],
-	max_completion_tokens=groq["MAX_COMPLETION_TOKENS"],
-	top_p=groq["TOP_P"],
-	stream=groq["STREAM"],
-	stop=groq["STOP"],
-	)
-	return completion.choices[0].message.content
+    if groq["STREAM"]:
+        pass
     else:
-	return ""
+        client = Groq(api_key=groq["YOUR_SECRET_GROQ_TOKEN"])
+        completion = client.chat.completions.create(
+        model=groq["MODEL"],
+        messages=groq["MESSAGES"],
+        temperature=groq["TEMPERATURE"],
+        max_completion_tokens=groq["MAX_COMPLETION_TOKENS"],
+        top_p=groq["TOP_P"],
+        stream=groq["STREAM"],
+        stop=groq["STOP"],
+        )
+        return completion.choices[0].message.content
 
 if __name__=="__main__":
     uvicorn.run(app, host=HOST, port=PORT)
