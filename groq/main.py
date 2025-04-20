e67eefa384beba5ebf77981f432fef662f0afe6b
A1ex Ma <145852894+A1eksMa@users.noreply.github.com>
2025-04-21 01:55:44 +0600

    Update main.py

--- groq/main.py
+++ groq/main.py
@@ -38,14 +38,22 @@ def groq_api(groq: dict = default_groq):
         return completion.choices[0].message.content
 
 @app.get("/groq_single_prompt")
-def groq_single_prompt(prompt: str, groq: dict = default_groq):
-    if groq["STREAM"]:
-        pass
-    else:
+def groq_single_prompt(prompt: str):
+    try:
+	global YOUR_SECRET_GROQ_TOKEN
+        groq = {"YOUR_SECRET_GROQ_TOKEN" : YOUR_SECRET_GROQ_TOKEN,
+            "MODEL" : "llama-3.3-70b-versatile",
+            "MESSAGES" : list({'role' : 'user', 'content' : prompt}),
+            "TEMPERATURE" : 1,
+	    "MAX_COMPLETION_TOKENS": 1024,
+	    "TOP_P": 1,
+            "STREAM": False,
+            "STOP": None,
+           }
         client = Groq(api_key=groq["YOUR_SECRET_GROQ_TOKEN"])
         completion = client.chat.completions.create(
             model=groq["MODEL"],
-            messages=groq["MESSAGES"].append({'role' : 'user', 'content' : prompt}),
+            messages=groq["MESSAGES"],
             temperature=groq["TEMPERATURE"],
             max_completion_tokens=groq["MAX_COMPLETION_TOKENS"],
             top_p=groq["TOP_P"],
@@ -53,6 +61,8 @@ def groq_single_prompt(prompt: str, groq: dict = default_groq):
             stop=groq["STOP"],
         )
         return completion.choices[0].message.content
+    except:
+        return "The service is temporarily unavailable."
 	    
 if __name__=="__main__":
     uvicorn.run(app, host=HOST, port=PORT)
