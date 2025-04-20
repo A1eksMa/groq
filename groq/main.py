b7b350362d08966218844582c790cdfce54a628e
A1ex Ma <145852894+A1eksMa@users.noreply.github.com>
2025-04-21 01:38:27 +0600

    Update main.py

--- groq/main.py
+++ groq/main.py
@@ -37,5 +37,34 @@ def groq_api(groq: dict = default_groq):
         )
         return completion.choices[0].message.content
 
+@app.get("/groq_single_prompt")
+def groq_api(prompt: str, groq: dict = default_groq):
+    """
+    Example of usage.
+    Set `groq` variable:
+    groq = {"YOUR_SECRET_GROQ_TOKEN" : YOUR_SECRET_GROQ_TOKEN,
+            "MODEL" : "llama-3.3-70b-versatile",
+            "MESSAGES" : list(),
+            "TEMPERATURE" : 1,
+	    "MAX_COMPLETION_TOKENS": 1024,
+	    "TOP_P": 1,
+            "STREAM": False,
+            "STOP": None,
+           }
+    """
+    if groq["STREAM"]:
+        pass
+    else:
+        client = Groq(api_key=groq["YOUR_SECRET_GROQ_TOKEN"])
+        completion = client.chat.completions.create(
+        model=groq["MODEL"],
+        messages=groq["MESSAGES"].append({'role' : 'user', 'content' : prompt}),
+        temperature=groq["TEMPERATURE"],
+        max_completion_tokens=groq["MAX_COMPLETION_TOKENS"],
+        top_p=groq["TOP_P"],
+        stream=groq["STREAM"],
+        stop=groq["STOP"],
+        )
+        return completion.choices[0].message.content.replace('\\n', '\n')[1:-1]
 if __name__=="__main__":
     uvicorn.run(app, host=HOST, port=PORT)
