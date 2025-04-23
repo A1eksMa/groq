dcfed059db853d356beb24742f84199668a5e2d4
A1ex Ma <145852894+A1eksMa@users.noreply.github.com>
2025-04-23 17:08:17 +0600

    streeeam

--- groq/main.py
+++ groq/main.py
@@ -8,26 +8,10 @@ import asyncio
 
 app = FastAPI()
 
-async def handle_stream(groq):
+async def handle_groq(groq):
     client = AsyncGroq(api_key=groq["YOUR_SECRET_GROQ_TOKEN"])
-    async for message in client.chat.completions.create(
-            model=groq["MODEL"],
-            messages=groq["MESSAGES"],
-            temperature=groq["TEMPERATURE"],
-            max_completion_tokens=groq["MAX_COMPLETION_TOKENS"],
-            top_p=groq["TOP_P"],
-            stream=groq["STREAM"],
-            stop=groq["STOP"],
-        ):
-        yield message.choices[0].message.content
-
-@app.post("/")
-async def groq_api(groq: dict = default_groq):
     if groq["STREAM"]:
-        return Response(handle_stream(groq), media_type="text/event-stream")
-    else:
-        client = AsyncGroq(api_key=groq["YOUR_SECRET_GROQ_TOKEN"])
-        completion = await client.chat.completions.create(
+        async for message in client.chat.completions.create(
                 model=groq["MODEL"],
                 messages=groq["MESSAGES"],
                 temperature=groq["TEMPERATURE"],
@@ -35,8 +19,41 @@ async def groq_api(groq: dict = default_groq):
                 top_p=groq["TOP_P"],
                 stream=groq["STREAM"],
                 stop=groq["STOP"],
-                )
-        return completion.choices[0].message.content
+            ):
+            yield message.choices[0].message.content
+    else:
+        completion = await client.chat.completions.create(
+            model=groq["MODEL"],
+            messages=groq["MESSAGES"],
+            temperature=groq["TEMPERATURE"],
+            max_completion_tokens=groq["MAX_COMPLETION_TOKENS"],
+            top_p=groq["TOP_P"],
+            stream=groq["STREAM"],
+            stop=groq["STOP"],
+        )
+        yield completion.choices[0].message.content
+
+async def stream_response(groq):
+    gen = handle_groq(groq)
+    while True:
+        try:
+            chunk = await gen.__anext__()
+            if chunk == "":
+                break
+            yield chunk
+        except StopAsyncIteration:
+            break
+
+@app.post("/")
+async def groq_api(groq: dict = default_groq):
+    if groq["STREAM"]:
+        async def stream():
+            async for chunk in stream_response(groq):
+                yield chunk + "\\n"
+        return Response(stream(), media_type="text/event-stream")
+    else:
+        gen = handle_groq(groq)
+        return {"result": await gen.__anext__()}
 
 @app.get("/groq_single_prompt")
 async def groq_single_prompt(prompt: str):
