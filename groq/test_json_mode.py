b8bbdb7e9932ae02eb91b0980d368df20d5c01d7
A1ex Ma <145852894+A1eksMa@users.noreply.github.com>
2025-04-23 23:13:21 +0600

    Create test_json_mode.py

---
+++ groq/test_json_mode.py
@@ -0,0 +1,27 @@
+from groq import AsyncGroq
+from config import default_groq
+from config import YOUR_SECRET_GROQ_TOKEN
+import asyncio
+
+groq = default_groq
+groq["STREAM"] = True
+groq["MESSAGES"].append({'role' : 'system', 'content' : 'Always return me json, that looks like a:{"ANSWER": text string with your message}'})
+groq["MESSAGES"].append({'role' : 'user', 'content' : 'Hello'})
+
+async def main():
+    client = AsyncGroq(api_key=groq["YOUR_SECRET_GROQ_TOKEN"])
+    completion = await client.chat.completions.create(
+        model=groq["MODEL"],
+        messages=groq["MESSAGES"],
+        temperature=groq["TEMPERATURE"],
+        max_completion_tokens=groq["MAX_COMPLETION_TOKENS"],
+        top_p=groq["TOP_P"],
+        stream=groq["STREAM"],
+        response_format={'type': 'json_object'},
+        stop=groq["STOP"],
+    )
+
+    async for chunk in completion:
+        print(chunk.choices[0].delta.content or "", end="")
+
+asyncio.run(main())
