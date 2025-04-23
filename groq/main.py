4e40618b51544f26293c25bf056d9676facc7ac9
A1ex Ma <145852894+A1eksMa@users.noreply.github.com>
2025-04-23 20:17:30 +0600

    Union endpoints with stream and not stream cases

--- groq/main.py
+++ groq/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 @app.post("/")
 async def groq_api(groq: dict = default_groq):
-    client = AsyncGroq(api_key=groq["YOUR_SECRET_GROQ_TOKEN"])
+    client = AsyncGroq(api_key=groq[YOUR_SECRET_GROQ_TOKEN])
     completion = await client.chat.completions.create(
             model=groq["MODEL"],
             messages=groq["MESSAGES"],
@@ -21,28 +21,15 @@ async def groq_api(groq: dict = default_groq):
             stream=groq["STREAM"],
             stop=groq["STOP"],
         )
-    return completion.choices[0].message.content
-
-
-@app.post("/stream_true")
-async def groq_api_stream_true(groq: dict = default_groq):
-    groq["STREAM"] = True
-    client = AsyncGroq(api_key=groq["YOUR_SECRET_GROQ_TOKEN"])
-    completion = await client.chat.completions.create(
-            model=groq["MODEL"],
-            messages=groq["MESSAGES"],
-            temperature=groq["TEMPERATURE"],
-            max_completion_tokens=groq["MAX_COMPLETION_TOKENS"],
-            top_p=groq["TOP_P"],
-            stream=True,
-            stop=groq["STOP"],
-        )
-
-    async def stream(completion):
-        async for chunk in completion:
-            yield chunk.choices[0].delta.content or ""
-
-    return StreamingResponse(stream(completion), media_type="text/plain")
+    
+    if groq["STREAM"]:
+        async def stream(completion):
+            async for chunk in completion:
+                yield chunk.choices[0].delta.content or ""
+        
+        return StreamingResponse(stream(completion), media_type="text/plain")
+    else:
+        return completion.choices[0].message.content
 
 @app.get("/groq_single_prompt")
 async def groq_single_prompt(prompt: str):
