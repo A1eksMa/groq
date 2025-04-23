3797b1820d2ccbdcb0069f0d4dfd40ea4d51bba0
A1ex Ma <145852894+A1eksMa@users.noreply.github.com>
2025-04-23 17:31:29 +0600

    Create test_groq_stream_mode.py

---
+++ groq/test_groq_stream_mode.py
@@ -0,0 +1,22 @@
+from groq import Groq
+from config import default_groq
+from config import YOUR_SECRET_GROQ_TOKEN
+#import asyncio
+
+groq = default_groq
+groq["STREAM"] = True
+groq["MESSAGES"].append({'role' : 'user', 'content' : 'How to write the Hello World! in java? Give me example and explain  me how it works.'})
+
+client = Groq(api_key=groq["YOUR_SECRET_GROQ_TOKEN"])
+completion = client.chat.completions.create(
+        model=groq["MODEL"],
+        messages=groq["MESSAGES"],
+        temperature=groq["TEMPERATURE"],
+        max_completion_tokens=groq["MAX_COMPLETION_TOKENS"],
+        top_p=groq["TOP_P"],
+        stream=groq["STREAM"],
+        stop=groq["STOP"],
+        )
+
+for chank in completion:
+    print(chunk.choices[0].delta.content or "", end="")
