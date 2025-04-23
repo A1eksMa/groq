6bc46cb08669c90e1d85cefc4406ff4fe1963547
A1ex Ma <145852894+A1eksMa@users.noreply.github.com>
2025-04-23 16:11:46 +0600

    Update main.py

--- groq/main.py
+++ groq/main.py
@@ -1,5 +1,5 @@
 from groq import AsyncGroq
-from fastapi import FastAPI
+from fastapi import FastAPI, StreamingResponse
 import uvicorn
 from config import HOST, PORT
 from config import default_groq
@@ -8,10 +8,12 @@ import asyncio
 
 app = FastAPI()
 
-@app.post("/")
-async def groq_api(groq: dict = default_groq):
+async def handle_stream(groq):
+    """
+    Handle streaming response from Groq API.
+    """
     client = AsyncGroq(api_key=groq["YOUR_SECRET_GROQ_TOKEN"])
-    completion = await client.chat.completions.create(
+    async for message in client.chat.completions.create(
             model=groq["MODEL"],
             messages=groq["MESSAGES"],
             temperature=groq["TEMPERATURE"],
@@ -19,8 +21,25 @@ async def groq_api(groq: dict = default_groq):
             top_p=groq["TOP_P"],
             stream=groq["STREAM"],
             stop=groq["STOP"],
-        )
-    return completion.choices[0].message.content
+        ):
+        yield message.choices[0].message.content
+
+@app.post("/")
+async def groq_api(groq: dict = default_groq):
+    if groq["STREAM"]:
+        return StreamingResponse(handle_stream(groq), media_type="text/event-stream")
+    else:
+        client = AsyncGroq(api_key=groq["YOUR_SECRET_GROQ_TOKEN"])
+        completion = await client.chat.completions.create(
+                model=groq["MODEL"],
+                messages=groq["MESSAGES"],
+                temperature=groq["TEMPERATURE"],
+                max_completion_tokens=groq["MAX_COMPLETION_TOKENS"],
+                top_p=groq["TOP_P"],
+                stream=groq["STREAM"],
+                stop=groq["STOP"],
+                )
+        return completion.choices[0].message.content
 
 @app.get("/groq_single_prompt")
 async def groq_single_prompt(prompt: str):
